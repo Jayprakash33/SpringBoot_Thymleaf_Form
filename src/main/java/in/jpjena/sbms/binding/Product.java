e112f172992576f0f23c3f3e8058ee8331e43c88
Jayprakash <jayprakashjena354@gmail.com>
2025-07-11 02:28:06 +0530

    form Validation Added

--- src/main/java/in/jpjena/sbms/binding/Product.java
+++ src/main/java/in/jpjena/sbms/binding/Product.java
@@ -1,11 +1,18 @@
 package in.jpjena.sbms.binding;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Product {
 
+	@NotNull(message = "Product Id Is Mandatory")
 	private Integer pid;
+	@NotBlank(message = " Product Name Is Mandatory")
+	@Size(min = 3 ,max = 8 ,message = "Size Must Be Between 3 to 8 Characters")
 	private String pname;
+	@NotNull(message = " Product Price Is Manadatory")
 	private Double price;
 }
--- src/main/java/in/jpjena/sbms/controller/ProductController.java
+++ src/main/java/in/jpjena/sbms/controller/ProductController.java
@@ -2,28 +2,31 @@ package in.jpjena.sbms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.jpjena.sbms.binding.Product;
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
 
 	@GetMapping("/")
 	public String getForm(Model model) {
-		model.addAttribute("product",new Product());
+		model.addAttribute("product", new Product());
 		return "index";
 	}
-	
+
 	@PostMapping("/product")
-	public String handleSubmitBtn(Product product,Model model) {
-		
+	public String handleSubmitBtn(@Valid Product product, BindingResult result, Model model) {
+
 		System.out.println(product);
-		
-		//logic to save in db
-		model.addAttribute("msg","Product Saved");
-		
+		if (result.hasErrors()) {
+			model.addAttribute("msg", "Product Data Is Not Provided In Correct Manner");
+		} else {
+			model.addAttribute("msg", "Product Saved");
+		}
 		return "index";
 	}
 }
